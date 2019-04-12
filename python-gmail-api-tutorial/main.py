4385c44075bcedcc51e92bb8027f8501ae066821
Jess Mark Sevilleja <jessmark.sevilleja@bayviewtechnology.com>
2019-04-12 17:07:55 +0800

    client_id

--- python-gmail-api-tutorial/main.py
+++ python-gmail-api-tutorial/main.py
@@ -1,5 +1,5 @@
-
 from __future__ import print_function
+import send_email
 import httplib2
 import os
 
@@ -15,6 +15,8 @@ except ImportError:
     flags = None
 
 import auth
+
+
 def get_labels():
     results = service.users().labels().list(userId='me').execute()
     labels = results.get('labels', [])
@@ -26,17 +28,18 @@ def get_labels():
         for label in labels:
             print(label['name'])
 
+
 SCOPES = 'https://mail.google.com/'
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'client_id.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
-authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+authInst = auth.auth(SCOPES, CLIENT_SECRET_FILE, APPLICATION_NAME)
 credentials = authInst.get_credentials()
 
 http = credentials.authorize(httplib2.Http())
 service = discovery.build('gmail', 'v1', http=http)
 
-import send_email
 
 sendInst = send_email.send_email(service)
-message = sendInst.create_message_with_attachment('test17291729@gmail.com','test17291729@gmail.com','Testing 123','Hi there, This is a test from Python!', 'image.jpg' )
-sendInst.send_message('me',message)
+message = sendInst.create_message_with_attachment(
+    'jessmark.sevilleja@bayviewtechnology.com', 'jesssevilleja@gmail.com', 'Testing 123', 'Hi there, This is a test from Python!', 'image.jpg')
+sendInst.send_message('me', message)
